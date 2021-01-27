ff416e06526d581f392dc8141fbb2f170d36287a
Saurab Gami <saurabgami977@gmail.com>
2021-01-27 18:40:18 +0545

    js array method .includes applied

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const checkPhone = (number) => {
         ? { valid: false, reason: 'Landline' }
         : num[0] != 9 && num[1] != 8 ? console.log({ valid: false, reason: 'Random' })
             : num.length < 10 || num.length > 10 ? { valid: false, reason: 'Invalid Length' }
-                : num[2] == 3 || num[2] == 7 || num[2] == 8 || num[2] == 9 ? { valid: false, reason: "Invalid Nepali Carrier" }
+                : ['3', '7', '8', '9'].includes(num[2]) ? { valid: false, reason: "Invalid Nepali Carrier" }
                     : { valid: true }
 }
-console.log(checkPhone('016638367'))
-console.log(checkPhone('65432768'))
-console.log(checkPhone('988722354264'))
-console.log(checkPhone('9887264'))
+// console.log(checkPhone('016638367'))
+// console.log(checkPhone('65432768'))
+// console.log(checkPhone('988722354264'))
+// console.log(checkPhone('9887264'))
 console.log(checkPhone('9887223264'))
 console.log(checkPhone('9897223264'))
-console.log(checkPhone('9861484326'))
+// console.log(checkPhone('9861484326'))
