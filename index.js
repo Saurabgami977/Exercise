4db5f539942d02ed773459c0d8ec25a74172d070
Saurab Gami <saurabgami977@gmail.com>
2021-01-27 15:45:40 +0545

    made more clear

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const checkPhone = (number) => {
     let num = [...number]
     return num[0] == 0
         ? { valid: false, reason: 'Landline' }
-        : num.length < 10 || num.length > 10 ? { valid: false, reason: 'Invalid Length' }
-            : num[0] === '9' && num[1] === '8' && num[2] === '3' || num[2] === '7' || num[2] === '8' || num[2] === '9' ? { valid: false, reason: "Invalid Nepali Carrier" }
-                : { valid: true }
+        : num[0] != 9 && num[1] != 8 ? console.log({ valid: false, reason: 'Random' })
+            : num.length < 10 || num.length > 10 ? { valid: false, reason: 'Invalid Length' }
+                : num[2] === '3' || num[2] === '7' || num[2] === '8' || num[2] === '9' ? { valid: false, reason: "Invalid Nepali Carrier" }
+                    : { valid: true }
 }
-console.log(checkPhone('042236712'))
+console.log(checkPhone('9842236712'))
