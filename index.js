59e91fb4a99c7ddc65bce317be52d4585222279c
Saurab Gami <saurabgami977@gmail.com>
2021-01-27 15:56:13 +0545

    made more efficient

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ const checkVersion = (firstNumber, secondNumber) => {
 
 const checkPhone = (number) => {
     let num = [...number]
-    return num[0] == 0
+    return num[0] == 0 && num.length == 9
         ? { valid: false, reason: 'Landline' }
         : num[0] != 9 && num[1] != 8 ? console.log({ valid: false, reason: 'Random' })
             : num.length < 10 || num.length > 10 ? { valid: false, reason: 'Invalid Length' }
-                : num[2] === '3' || num[2] === '7' || num[2] === '8' || num[2] === '9' ? { valid: false, reason: "Invalid Nepali Carrier" }
+                : num[2] == 3 || num[2] == 7 || num[2] == 8 || num[2] == 9 ? { valid: false, reason: "Invalid Nepali Carrier" }
                     : { valid: true }
 }
-console.log(checkPhone('9842236712'))
+console.log(checkPhone('016638367'))
+console.log(checkPhone('65432768'))
+console.log(checkPhone('988722354264'))
+console.log(checkPhone('9887264'))
+console.log(checkPhone('9887223264'))
+console.log(checkPhone('9897223264'))
+console.log(checkPhone('9861484326'))
