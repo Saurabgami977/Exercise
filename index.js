450d7581b893ba9f7a286b7bda7466110eb5c2e7
Saurab Gami <saurabgami977@gmail.com>
2021-01-27 15:40:34 +0545

    second function added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const checkVersion = (firstNumber, secondNumber) => {
     return firstNumber > secondNumber ?
         console.log(firstNumber, '>', secondNumber)
-            : firstNumber < secondNumber
-                ? console.log(secondNumber, '>', firstNumber)
-                : console.log('equal');
+        : firstNumber < secondNumber
+            ? console.log(secondNumber, '>', firstNumber)
+            : console.log('equal');
 }
 
-checkVersion('4.2.1', '3.2.1')
+// checkVersion('4.2.9', '0.9.1')
+
+const checkPhone = (number) => {
+    let num = [...number]
+    return num[0] == 0
+        ? { valid: false, reason: 'Landline' }
+        : num.length < 10 || num.length > 10 ? { valid: false, reason: 'Invalid Length' }
+            : num[0] === '9' && num[1] === '8' && num[2] === '3' || num[2] === '7' || num[2] === '8' || num[2] === '9' ? { valid: false, reason: "Invalid Nepali Carrier" }
+                : { valid: true }
+}
+console.log(checkPhone('042236712'))
